56cfbd9ac2f854b05ddd4eb585a4d18f89472137
Toby Dylan Hocking <tdhock5@gmail.com>
2017-05-10 16:08:06 -0400

    pointer moves separate figures

--- figure-moves.R
+++ figure-moves.R
@@ -3,7 +3,7 @@ source("packages.R")
 load("moves.RData")
 
 moves[, round.log10.limits := round(log10(total.finite), 1)]
-
+moves[, hinge.loss := ifelse(loss=="hinge", "linear", "square")]
 dmoves <- moves[, list(
   max.moves=max(max.moves),
   min.max=min(max.moves),
@@ -18,11 +18,115 @@ dmoves <- moves[, list(
   median.max=median(max.moves.per.limit),
   q75.max=quantile(max.moves.per.limit, 0.75),
   q25.max=quantile(max.moves.per.limit, 0.25)
-), by=list(total.finite=10^round.log10.limits, loss)]
+), by=list(total.finite=10^round.log10.limits, loss, hinge.loss)]
+
+gg.max <- ggplot()+
+  theme_bw()+
+  scale_x_log10(
+    "number of outputs (finite interval limits)")+
+  geom_line(aes(
+    total.finite, median.max,
+    color=hinge.loss),
+    size=1,
+    data=dmoves)+
+  geom_ribbon(aes(
+    total.finite,
+    fill=hinge.loss,
+    ymin=q25.max, ymax=q75.max),
+    data=dmoves,
+    alpha=0.5)+
+  scale_y_continuous("max pointer moves", limits=c(0,9), breaks=0:9)
+print(gg.max)
+pdf("figure-moves-max.pdf", 4, 3)
+print(gg.max)
+dev.off()
+
+gg <- ggplot()+
+  theme_bw()+
+  scale_x_log10(
+    "number of outputs (finite interval limits)")+
+  geom_line(aes(
+    total.finite, median.moves,
+    color=hinge.loss),
+    size=1,
+    data=dmoves)+
+  geom_ribbon(aes(
+    total.finite,
+    fill=hinge.loss,
+    ymin=q25.moves, ymax=q75.moves),
+    data=dmoves,
+    alpha=0.5)+
+  scale_y_continuous("average pointer moves", limits=c(0,1))
+print(gg)
+pdf("figure-moves-average.pdf", 4, 3)
+print(gg)
+dev.off()
 
 y <- function(yvar){
   factor(yvar, c("max", "average"))
 }
+gg <- ggplot()+
+  theme_bw()+
+  facet_wrap("y", scales="free")+
+  scale_x_log10("number of outputs (finite interval limits)")+
+  ## geom_line(aes(
+  ##   total.finite, mean.moves,
+  ##   color=loss),
+  ##   size=1,
+  ##   data=data.table(dmoves, y="average"))+
+  ## geom_ribbon(aes(
+  ##   total.finite,
+  ##   fill=loss,
+  ##   ymin=mean.moves-sd.moves, ymax=mean.moves+sd.moves),
+  ##   data=data.table(dmoves, y="average"),
+  ##   alpha=0.5)+
+  ## geom_line(aes(
+  ##   total.finite, mean.max,
+  ##   color=loss),
+  ##   size=1,
+  ##   data=data.table(dmoves, y="max"))+
+  ## geom_ribbon(aes(
+  ##   total.finite,
+  ##   fill=loss,
+  ##   ymin=mean.max-sd.max, ymax=mean.max+sd.max),
+  ##   data=data.table(dmoves, y="max"),
+  ##   alpha=0.5)+
+  geom_line(aes(
+    total.finite, median.moves,
+    color=loss),
+    size=1,
+    data=data.table(dmoves, y=y("average")))+
+  geom_ribbon(aes(
+    total.finite,
+    fill=loss,
+    ymin=q25.moves, ymax=q75.moves),
+    data=data.table(dmoves, y=y("average")),
+    alpha=0.5)+
+  geom_line(aes(
+    total.finite, median.max,
+    color=loss),
+    size=1,
+    data=data.table(dmoves, y=y("max")))+
+  geom_ribbon(aes(
+    total.finite,
+    fill=loss,
+    ymin=q25.max, ymax=q75.max),
+    data=data.table(dmoves, y=y("max")),
+    alpha=0.5)+
+  ## geom_line(aes(
+  ##   total.finite, max.moves.per.limit,
+  ##   color=loss),
+  ##   data=data.table(dmoves, y="max"))+
+  scale_y_continuous("pointer moves", breaks=0:10)+
+  geom_blank(aes(total.finite, moves), data=data.table(
+    total.finite=100,
+    moves=c(0, 1, 0, 10),
+    y=y(c("average", "average", "max", "max"))))
+print(gg)
+pdf("figure-moves.pdf", 4, 3)
+print(gg)
+dev.off()
+
 gg <- ggplot()+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
@@ -84,3 +188,4 @@ gg <- ggplot()+
 png("figure-moves.png")
 print(gg)
 dev.off()
+
