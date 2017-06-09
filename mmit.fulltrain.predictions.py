5c586d0916a36a40841bae85e6dbc3c33a833ccf
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-06-09 09:33:14 -0400

    A few changes

--- mmit.fulltrain.predictions.py
+++ mmit.fulltrain.predictions.py
@@ -15,12 +15,12 @@ class Dataset(object):
     def __init__(self, path):
         self.path = path
         feature_data = pd.read_csv(join(path, "features.csv"))
-        self.X = feature_data.values
+        self.X = feature_data.values.astype(np.float)
         self.X.flags.writeable = False
         self.feature_names = feature_data.columns.values
         self.feature_names.flags.writeable = False
         del feature_data
-        self.y = pd.read_csv(join(path, "targets.csv")).values
+        self.y = pd.read_csv(join(path, "targets.csv")).values.astype(np.float)
         self.y.flags.writeable = False
         self.folds = pd.read_csv(join(path, "folds.csv")).values.reshape(-1, )
         self.folds.flags.writeable = False
@@ -56,6 +56,7 @@ def mse_scorer(estimator, X, y):
 def save_predictions(estimator, method, dataset, predictions_path):
     open(join(predictions_path, method, dataset.name, "predictions.fulltrain.csv"), "w")\
         .write("pred.log.penalty\n" + "\n".join(str(p) for p in estimator.predict(dataset.X)))
+    print estimator
     json.dump(estimator.get_params(), open(join(predictions_path, method, dataset.name, "parameters.fulltrain.json"), "w"))
 
 
@@ -73,20 +74,18 @@ if __name__ == "__main__":
         print "{0:d}/{1:d}: {2!s}".format(i + 1, len(datasets), d.name)
 
         # Determine the margin grid
-        sorted_limits = d.y.flatten()
+        sorted_limits = d.y.flatten().astype(np.float)
         sorted_limits = sorted_limits[~np.isinf(sorted_limits)]
         sorted_limits.sort()
         range_max = sorted_limits.max() - sorted_limits.min()
         range_min = np.diff(sorted_limits)
         range_min = range_min[range_min > 0].min()
-        param_template["margin"] = [0.] + np.logspace(np.log10(range_min),
-                                                      np.log10(range_max),
-                                                      n_margin_values).tolist()
+        param_template["margin"] = [0.] + np.logspace(np.log10(range_min), np.log10(range_max), n_margin_values).tolist()
 
         print ".... linear hinge"
         method = "mmit.linear.hinge.pruning"
         params = dict(param_template)
-        params["loss"] = "hinge"
+        params["loss"] = ["hinge"]
         if not exists(join(predictions_path, method, d.name)) or \
                 not exists(join(predictions_path, method, d.name, "predictions.fulltrain.csv")):
             try:
@@ -95,12 +94,22 @@ if __name__ == "__main__":
                 pass
             cv = GridSearchCV(estimator=MaxMarginIntervalTree(), param_grid=params, cv=10, n_jobs=n_cpu,
                               scoring=mse_scorer, pruning=True).fit(d.X, d.y)
+
+            for hps, metrics in cv.cv_results_:
+                print hps, metrics["cv"]
+
+            print "BEST:"
+            print cv.best_estimator_
+            print cv.best_params_
+            print cv.best_score_
+            exit()
+
             save_predictions(cv.best_estimator_, method, d, predictions_path)
 
         print ".... squared hinge"
         method = "mmit.squared.hinge.pruning"
         params = dict(param_template)
-        params["loss"] = "squared_hinge"
+        params["loss"] = ["squared_hinge"]
         if not exists(join(predictions_path, method, d.name)) or \
                 not exists(join(predictions_path, method, d.name, "predictions.fulltrain.csv")):
             try:
