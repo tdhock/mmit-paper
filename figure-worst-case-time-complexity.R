787e2f9033db3b54f8b6c7da22e4a8271f818605
Toby Dylan Hocking <tdhock5@gmail.com>
2017-08-11 14:39:06 -0400

    (-10)^n works too

--- figure-worst-case-time-complexity.R
+++ figure-worst-case-time-complexity.R
@@ -2,11 +2,13 @@ source("packages.R")
 
 size <- 20
 n <- 1:size
-n.vec <- (-n)^n
+n.vec <- (-n)^n ## N-2 moves.
+n.vec <- (-10)^n ## N-2 moves as well.
 out.mat <- cbind(
   ifelse(n.vec<0, -Inf, n.vec),
   ifelse(n.vec<0, n.vec, Inf))
 (result <- mmit::compute_optimal_costs(out.mat, 1, "square"))
+plot(result$moves)
 pdf("figure-worst-case-time-complexity.pdf")
 plot(result$moves)
 dev.off()
